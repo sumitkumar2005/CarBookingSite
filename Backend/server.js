a0a47176d0b866f696fa8b3ffde8b68ff6fc34bf
sumit <sumitjha4552@gmail.com>
2024-12-30 11:48:02 +0530

    Intial

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+import http from 'http'
+import app  from './app.js'
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT||3000
+const server = http.createServer(app);
+
+server.listen(port,()=>{
+    console.log(`Server is Listening at port ${port}`)
+});
